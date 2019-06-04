1c8546de2adec518e37b2c15653d14474ae6c7c9
Luboremo <luboremenar@gmail.com>
2019-06-04 05:14:19 +0200

    Update EDG-ssss.sh

--- EDG-ssss.sh
+++ EDG-ssss.sh
@@ -1,2 +1,2 @@
-cat mnemonic.txt | ssss-split -t3 -n5 > shamir.txt								# Encrypts mnemonic with Shamirs secret sharing scheme, -t <minimum number of shares needed to reconstruct the secret> -n <number of shares>								
+cat mnemonic.txt | ssss-split -t3 -n5 > shamir.txt						# Encrypts mnemonic with Shamirs secret sharing scheme, -t <minimum number of shares needed to reconstruct the secret> -n <number of shares>								
 cat shamir.txt
