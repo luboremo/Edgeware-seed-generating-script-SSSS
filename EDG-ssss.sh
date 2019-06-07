0352c2fad7937df22bbdd29ad1f8ed043069a7a7
Luboremo <luboremenar@gmail.com>
2019-06-07 03:03:25 +0200

    Update EDG-ssss.sh

--- EDG-ssss.sh
+++ EDG-ssss.sh
@@ -1,2 +1,2 @@
 cat mnemonic.txt | ssss-split -t3 -n5 > shamir.txt						
-cat shamir.txt
+sed -n '1,2p' shamir.txt > 1stShare.txt | sed -n '3p' shamir.txt > 2ndShare.txt | sed -n '4p' shamir.txt > 3rdShare.txt | sed -n '5p' shamir.txt > 4thShare.txt | sed -n '6p' shamir.txt > 5thShare.txt
